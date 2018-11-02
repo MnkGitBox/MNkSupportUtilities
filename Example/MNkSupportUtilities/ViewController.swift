b2074bc3388d73b9c9cb9f30ec210229cb6e59df
MNk_Dev <mnk_dev@Azbow-iMac-27.local>
2018-11-02 16:00:50 +0530

    Initial commit

---
+++ Example/MNkSupportUtilities/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MNkSupportUtilities
+//
+//  Created by m.nadeeshan@yahoo.co.uk on 11/02/2018.
+//  Copyright (c) 2018 m.nadeeshan@yahoo.co.uk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
