77008d0eec4db716493578eb5d33cfda732c7b03
Malith Nadeeshan <MNk_iMac@Maliths-Macbook-Pro.local>
2020-12-10 21:33:11 +0530

    updated to tag 1.10

--- Classes/Extensions.swift
+++ Classes/Extensions.swift
@@ -588,3 +588,71 @@ public extension NSObject {
     }
 }
 
+/**
+ Converting between CGImagePropertyOrientation and UIImageOrientation
+ -
+ The CGImagePropertyOrientation type covers the same set of orientation names available in from the UIImage.Orientation type, but the underlying numeric values of each type do not match. (For example, the "left mirrored" orientation has an underlying value of 5 in CGImagePropertyOrientation, but an underlying value of 7 in UIImage.Orientation.) If you have an orientation value in one type and need a semantically equivalent value in the other, use a function such as those below to produce the same-named value in the other type:
+ */
+public extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+            case .up: self = .up
+            case .upMirrored: self = .upMirrored
+            case .down: self = .down
+            case .downMirrored: self = .downMirrored
+            case .left: self = .left
+            case .leftMirrored: self = .leftMirrored
+            case .right: self = .right
+            case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    var name: String {
+        switch self {
+            case .up: return ".up"
+            case .upMirrored: return ".upMirrored"
+            case .down: return ".down"
+            case .downMirrored: return ".downMirrored"
+            case .left: return ".left"
+            case .leftMirrored: return ".leftMirrored"
+            case .right: return ".right"
+            case .rightMirrored: return ".rightMirrored"
+        }
+    }
+}
+
+
+public extension UIImage.Orientation {
+    init(_ cgOrientation: CGImagePropertyOrientation) {
+        switch cgOrientation {
+            case .up: self = .up
+            case .upMirrored: self = .upMirrored
+            case .down: self = .down
+            case .downMirrored: self = .downMirrored
+            case .left: self = .left
+            case .leftMirrored: self = .leftMirrored
+            case .right: self = .right
+            case .rightMirrored: self = .rightMirrored
+
+        }
+    }
+    
+    var name: String {
+        switch self {
+            case .up: return ".up"
+            case .upMirrored: return ".upMirrored"
+            case .down: return ".down"
+            case .downMirrored: return ".downMirrored"
+            case .left: return ".left"
+            case .leftMirrored: return ".leftMirrored"
+            case .right: return ".right"
+            case .rightMirrored: return ".rightMirrored"
+
+        @unknown default:
+            fatalError()
+        }
+    }
+}
+
